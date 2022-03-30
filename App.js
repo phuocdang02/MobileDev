88c2a3fa27a180858d1c6c096424e06a8dc32500
PhuocDeng <haphuocdang@gmail.com>
2022-03-30 21:58:57 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native'; */
 
-export default function App() {
+/* export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Dang</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
+
+
+import React, { Component } from 'react';
+import Hello from './components/HelloComponents';
+
+class App extends Component {
+  render() {
+    return (
+      <Hello name='TDK' />
+    );
+  }
+}
+export default App;
