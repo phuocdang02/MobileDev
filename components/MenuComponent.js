ecbf322bc7e88628c4f86450dba1685ade9cadc3
PhuocDeng <haphuocdang@gmail.com>
2022-04-12 09:24:31 +0700

    Navigation part 1

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import { ListItem, Avatar } from 'react-native-elements';
-import {FlatList,View} from 'react-native';
-import Dishdetail from './DishdetailComponent';
+import {FlatList} from 'react-native';
+import { DISHES } from '../shared/dishes';
 
 class Menu extends Component {
   constructor(props){
     super(props);
     this.state ={
-      selectedDish: null
+      //selectedDish: null
+      dishes: DISHES
     };
   }
   render() {
     return (
-      <View style={{flex:1}}>
-      <FlatList data={this.props.dishes}
+      <FlatList data={this.state.dishes}
         renderItem={({ item, index }) => this.renderMenuItem(item, index)}
         keyExtractor={item => item.id.toString()} />
-      <Dishdetail dish={this.state.selectedDish}/>
-      </View>
     );
   }
   renderMenuItem(item, index) {
+    const { navigate }=this.props.navigation;
     return (
-      <ListItem key={index} onPress={() => this.onDishSelect(item)}>
+      <ListItem key={index} onPress={()=> navigate('Dishdetail',{dishId:item.id})}>
         <Avatar source={require('./images/uthappizza.png')} />
         <ListItem.Content>
           <ListItem.Title>{item.name}</ListItem.Title>
@@ -31,9 +30,5 @@ class Menu extends Component {
       </ListItem>
     );
   };
-  onDishSelect(item){
-    //alert(item.id);
-    this.setState({selectedDish:item});
-  }
 }
 export default Menu;
